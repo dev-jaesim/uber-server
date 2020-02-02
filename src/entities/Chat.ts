fca31382fe1b51c00eb38183092b5f5d8d32d925
dev-jaesim <dev.jaesim@gmail.com>
2020-02-02 16:54:03 +1100

    chat and message entities done

---
+++ src/entities/Chat.ts
@@ -0,0 +1,27 @@
+import {
+    BaseEntity,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+
+import Message from "./Message";
+import User from "./User";
+
+@Entity()
+class Chat extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number;
+
+    @OneToMany(type => Message, message => message.chat)
+    messages: Message[];
+
+    @OneToMany(type => User, user => user.chat)
+    participants: User[];
+
+    @CreateDateColumn() createdAt: string;
+
+    @UpdateDateColumn() updatedAt: string;
+}
+export default Chat;
---
+++ src/entities/Message.ts
@@ -0,0 +1,31 @@
+import {
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+
+import Chat from "./Chat";
+import User from "./User";
+
+@Entity()
+class Message extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number;
+
+    @Column({ type: "text" })
+    text: string;
+
+    @ManyToOne(type => Chat, chat => chat.messages)
+    chat: Chat;
+
+    @ManyToOne(type => User, user => user.messages)
+    user: User;
+
+    @CreateDateColumn() createdAt: string;
+
+    @UpdateDateColumn() updatedAt: string;
+}
+export default Message;
